37cfffd657110fb2db113e374f28804bf6ff7621
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-25 16:17:54 +0530

    removed space

--- 03.variable.sh
+++ 03.variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1= Suresh
-PERSON2= Ramesh
+PERSON1=Raheem
+PERSON2=Ramesh
 
 echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1, I am fine. How are you doing?"
