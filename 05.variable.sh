136ae560ffba2524d341a1062f777420429985b9
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-25 20:06:28 +0530

    added password

--- 05.variable.sh
+++ 05.variable.sh
@@ -2,7 +2,17 @@
 
 echo "Please enter username::"
 
-read USERNAME
+read -s USERNAME
 
 echo "username entered is: $USERNAME"
+
+echo "Please enter Password::"
+
+read -s PASSWORD
+
+echo "Password entered is: $PASSWORD"
+
+
+
+
 
