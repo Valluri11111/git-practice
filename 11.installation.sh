6b228f6348cdde80d7c5f13aaaa6eb3a99674078
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-26 19:23:55 +0530

    Installation

--- 11.installation.sh
+++ 11.installation.sh
@@ -5,4 +5,12 @@ then
     echo "Please run the script with root privilages"
     exit 1
 fi
-     
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git not Installed, Going to Install it...."
+    dnf install git -y
+else
+    echo "Git already Installed, Nothing to do anything..."     
