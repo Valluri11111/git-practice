8b899adb132b8beca7a882769f9e205022faa88e
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-26 17:53:11 +0530

    Special Variable

--- 09.special-variables.sh
+++ 09.special-variables.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "All Variables Passed Script: $@"
 echo "Number of Variables Passed: $#"
+echo "Script name: $0"
+echo current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of Script running now: $$"
+sleep 100
+echo "PID of last background command: $!"
  
