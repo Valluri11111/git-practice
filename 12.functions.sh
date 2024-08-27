7df1e40d4a93c4b8a7f8528aeaca0cb06a9698c6
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-27 11:58:22 +0530

    Functions

--- 12.functions.sh
+++ 12.functions.sh
@@ -2,20 +2,24 @@
 
 USERID=$(id -u)
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root privilages"
+        exit 1
+    fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo "$2 is... Failed"
     else
         echo "$2 is... Success"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run the script with root privilages"
-    exit 1
-fi
+   fi
+   }
+   
+CHECK_ROOT
 
 dnf list installed git
 
