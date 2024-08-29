a2918efc4658a5f5851dbffabc46e7c441ffd43b
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-29 16:40:33 +0530

    delete-old-logs

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -18,3 +18,9 @@ fi
 # SOURCE_DIR =/home/ec2-user/logs
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files: $FILES"
+
+#IFS = Internal File Seperator, Empty will Ignore while space,  -r is not to ignore special characters like /
+while IFS= read -r line 
+do
+    echo "Deleting line: $line"
+done <<< $FILES
