81ca28138722a2816060e4e3c1d9b4388e7e81b8
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-29 16:19:07 +0530

    delete-old-logs

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -2,10 +2,19 @@
 
 SOURCE_DIR=/home/ec2-user/logs
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ -d $SOURCE_DIR ]
 then
-    echo "$SOURCE_DIR Exists"
+    echo "$SOURCE_DIR  $G Exists $N"
 else
-    echo "$SOURCE_DIR does not Exists"
+    echo "$SOURCE_DIR  $R Does not Exists $N"
     exit 1
 fi
+
+# SOURCE_DIR =/home/ec2-user/logs
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files: $Files"
