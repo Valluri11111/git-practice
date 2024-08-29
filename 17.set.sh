1fe43ef68c89f06292816104df98dd7187db3593
rajeshvalluri18 <rajvalluri11@gmail.com>
2024-08-29 13:13:55 +0530

    Set

--- 17.set.sh
+++ 17.set.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 set -e # Setting automatic exit, if we get error. set -ex is for debug
+
+failure(){
+    echo "Failed at: $1:$2"
+
+}
+
+trap ' failure "${LINENO}" "$BASH_COMMAND" ' ERR #ERR is the Error Signal
 echo "Hello World"
 echooo "Hello World"
